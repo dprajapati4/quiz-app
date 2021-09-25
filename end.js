1ca3608f7062f9780eac88de81ae4a12ace5bac5
Deep Prajapati <dprajap2@gmail.com>
2021-09-24 21:33:46 -0400

    Add score to local storage

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById("username")
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 
+
+const highScores = JSON.parse(localStorage.getItem("highScores"))
+console.log(JSON.parse(localStorage.getItem("highScores")))
 const mostRecentScore = localStorage.getItem("mostRecentScore")
 
 finalScore.innerText = mostRecentScore;
