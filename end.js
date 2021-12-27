78ea9b1bbebea3c0f7bc2f9d383d828c2816532b
Deep Prajapati <dprajap2@gmail.com>
2021-12-27 12:18:31 -0500

    Save high scores to local storage

--- end.js
+++ end.js
@@ -1,23 +1,31 @@
-const username = document.getElementById("username")
-const saveScoreBtn = document.getElementById("saveScoreBtn");
-const finalScore = document.getElementById("finalScore");
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const highScores = JSON.parse(localStorage.getItem("highScores"))
-console.log(JSON.parse(localStorage.getItem("highScores")))
-const mostRecentScore = localStorage.getItem("mostRecentScore")
+const MAX_HIGH_SCORES = 5;
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 finalScore.innerText = mostRecentScore;
 
-
-username.addEventListener("keyup",() =>{
-  console.log(username.value)
-  saveScoreBtn.disabled = !username.value
-})
-
+username.addEventListener('keyup', () => {
+  console.log(username.value);
+  saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = (e) => {
-console.log("i clicked save")
-e.preventDefault();
+  console.log('i clicked save');
+  e.preventDefault();
 
-}
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
+};
